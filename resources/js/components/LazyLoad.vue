3f0944fde7e8913a214efb057af4d6745cdbc911
vietvd13 <vuducviet0131@gmail.com>
2021-04-25 09:34:39 +0700

    [FE][Component] Create component LazyLoad

---
+++ resources/js/components/LazyLoad.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="lazyload" />
+</template>
+
+<script>
+export default {
+  name: 'LazyLoad',
+  data() {
+    return {
+      lazyload: null,
+    };
+  },
+  mounted() {
+    this.lazyload = new IntersectionObserver(([entry]) => {
+      if (entry && entry.isIntersecting) {
+        this.$emit('lazyload');
+      }
+    });
+
+    this.lazyload.observe(this.$el);
+  },
+};
+</script>
