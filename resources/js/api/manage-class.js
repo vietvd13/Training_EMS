fb7cc31e220e235d3858583393163f7b8f129e39
vietvd13 <vuducviet0131@gmail.com>
2021-04-27 15:09:09 +0700

    [FE][Manage Class] Create all function not message

--- resources/js/api/manage-class.js
+++ resources/js/api/manage-class.js
@@ -5,7 +5,11 @@ const urlAPI = {
   // Get list all course
   urlGETListCourse: template`/course`,
   // Get list all student
-  urlGETListTrainee: template`/user`,
+  urlGETListTrainee: template`/user/trainee-list`,
+  // Get list all class
+  urlGETListClass: template`/class`,
+  // Get a class
+  urlGETOneClass: template`/class/${'id'}`,
   // Post create a new class
   urlPOSTCreateClass: template`/class`,
   // Put update class
@@ -14,22 +18,30 @@ const urlAPI = {
   urlDELETEClass: template`/class/${'id'}`,
 };
 
-export function getListCourse(pram) {
-  return request.getRequest(urlAPI.urlGETListCourse(), pram);
+export function getListCourse(param) {
+  return request.getRequest(urlAPI.urlGETListCourse(), param);
 }
 
 export function getListTrainee() {
   return request.getRequest(urlAPI.urlGETListTrainee());
 }
 
-export function postCreateClass() {
-  return request.postRequest(urlAPI.urlPOSTCreateClass());
+export function getListClass(param) {
+  return request.getRequest(urlAPI.urlGETListClass(), param);
 }
 
-export function putUpdateClass() {
-  return request.putRequest(urlAPI.urlPUTUpdateClass());
+export function getOneClass(param) {
+  return request.getRequest(urlAPI.urlGETOneClass(param));
 }
 
-export function deleteClass() {
-  return request.deleteRequest(urlAPI.urlDELETEClass());
+export function postCreateClass(newClass) {
+  return request.postRequest(urlAPI.urlPOSTCreateClass(), newClass);
+}
+
+export function putUpdateClass(class_update, id) {
+  return request.putRequest(urlAPI.urlPUTUpdateClass(id), class_update);
+}
+
+export function deleteClass(id) {
+  return request.deleteRequest(urlAPI.urlDELETEClass(id));
 }
