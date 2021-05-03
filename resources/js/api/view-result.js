6aee81d993c9bc93bda75df7662717b2892dfc6f
vietvd13 <vuducviet0131@gmail.com>
2021-05-03 11:42:46 +0700

    [FE][View Result] Create filter

--- resources/js/api/view-result.js
+++ resources/js/api/view-result.js
@@ -6,6 +6,12 @@ const urlAPI = {
   urlGETListGradeTrainer: template`/grades`,
   // Get list grade (Trainee)
   urlGETListGradeTrainee: template`/grade`,
+  // Get list class
+  urlGETListClass: template`/user/class-list/${'id'}`,
+  // Get list course
+  urlGETListCourse: template`/course`,
+  // Get list test
+  urlGETListTest: template`/test`,
 };
 
 export function getListGradeTrainer(params) {
@@ -14,4 +20,16 @@ export function getListGradeTrainer(params) {
 
 export function getListGradeTrainee(params) {
   return request.getRequest(urlAPI.urlGETListGradeTrainee(), params);
+}
+
+export function getListClass(params) {
+  return request.getRequest(urlAPI.urlGETListClass(params));
+}
+
+export function getListCourse(params) {
+  return request.getRequest(urlAPI.urlGETListCourse(), params);
+}
+
+export function getListTest(params) {
+  return request.getRequest(urlAPI.urlGETListTest(), params);
 }
--- resources/js/views/ViewResult/index.vue
+++ resources/js/views/ViewResult/index.vue
@@ -2,25 +2,46 @@
   <div style="margin-top: 20px">
     <div class="zone-header-page">
       <b-row>
-        <b-col v-if="userRole !== ConstValue.ROLE.TRAINEE" sm="3">
-          <b-form-input v-model="isfilter.fullname" :placeholder="$t('views.view-result.search-fullname')" />
+        <b-col sm="3">
+          <b-form-input v-if="userRole !== ConstValue.ROLE.TRAINEE" v-model="isfilter.fullname" :placeholder="$t('views.view-result.search-fullname')" />
         </b-col>
 
         <b-col sm="3">
           <b-form-select v-model="isfilter.class" class="mb-3">
             <b-form-select-option :value="null">{{ $t('views.view-result.please-select-class') }}</b-form-select-option>
+            <b-form-select-option
+              v-for="(isClass, indexClass) in ListClass"
+              :key="indexClass"
+              :value="isClass.id"
+            >
+              {{ isClass.class_name }}
+            </b-form-select-option>
           </b-form-select>
         </b-col>
 
         <b-col sm="3">
           <b-form-select v-model="isfilter.course" class="mb-3">
             <b-form-select-option :value="null">{{ $t('views.view-result.please-select-course') }}</b-form-select-option>
+            <b-form-select-option
+              v-for="(course, indexCourse) in ListCourse"
+              :key="indexCourse"
+              :value="course.course_id"
+            >
+              {{ course.course_name }}
+            </b-form-select-option>
           </b-form-select>
         </b-col>
 
         <b-col sm="3">
           <b-form-select v-model="isfilter.test" class="mb-3">
             <b-form-select-option :value="null">{{ $t('views.view-result.please-select-test') }}</b-form-select-option>
+            <b-form-select-option
+              v-for="(test, indexTest) in ListTest"
+              :key="indexTest"
+              :value="test.test_id"
+            >
+              {{ test.test_name }}
+            </b-form-select-option>
           </b-form-select>
         </b-col>
       </b-row>
@@ -121,7 +142,7 @@
 import { IsEmptyOrWhiteSpace } from '@/utils/validate';
 
 // Import function call api
-import { getListGradeTrainer, getListGradeTrainee } from '@/api/view-result';
+import { getListGradeTrainer, getListGradeTrainee, getListClass, getListCourse, getListTest } from '@/api/view-result';
 
 // Import Const Value
 import ConstValue from '../../../../const/const_value.json';
@@ -154,12 +175,88 @@ export default {
       },
 
       ListResult: [],
+
+      ListClass: [],
+      ListCourse: [],
+      ListTest: [],
     };
+  },
+  computed: {
+    isFilterFullnameChange() {
+      return this.isfilter.fullname;
+    },
+    isFilterClassChange() {
+      return this.isfilter.class;
+    },
+    isFilterCourseChange() {
+      return this.isfilter.course;
+    },
+    isFilterTestChange() {
+      return this.isfilter.test;
+    },
+  },
+  watch: {
+    isFilterFullnameChange() {
+      this.handleWatchFilter();
+    },
+    isFilterClassChange() {
+      this.handleWatchFilter();
+      this.isfilter.course = null;
+      this.isfilter.test = null;
+    },
+    isFilterCourseChange() {
+      this.handleWatchFilter();
+      this.isfilter.test = null;
+    },
+    isFilterTestChange() {
+      this.handleWatchFilter();
+    },
   },
   created() {
     this.handleDoFilterByRole();
+    this.handleWatchFilter();
   },
   methods: {
+    handleWatchFilter() {
+      const PARAMS_FILTER_CLASS = {
+        'id': this.$store.getters.userId,
+      };
+
+      getListClass(PARAMS_FILTER_CLASS)
+        .then((response) => {
+          if (this.userRole === ConstValue.ROLE.TRAINEE) {
+            this.ListClass = response.classes;
+          } else {
+            this.ListClass = response.classes_trainer;
+          }
+
+          if (this.isfilter.class !== null) {
+            const PARAMS_FILTER_COURSE = {
+              'id': this.isfilter.class,
+            };
+
+            getListCourse(PARAMS_FILTER_COURSE)
+              .then((response) => {
+                this.ListCourse = response.data;
+
+                if (this.isfilter.class !== null && this.isfilter.course !== null) {
+                  const PARAMS_FILTER_TEST = {
+                    'class': this.isfilter.class,
+                    'course': this.isfilter.course,
+                  };
+
+                  getListTest(PARAMS_FILTER_TEST)
+                    .then((response) => {
+                      this.ListTest = response.data;
+                    });
+                }
+              });
+          }
+        });
+
+      this.handleDoFilterByRole();
+    },
+
     handleDoFilterByRole() {
       if (this.userRole === ConstValue.ROLE.TRAINEE) {
         this.handleGetGradeTrainee();
